9dd026861c643aaaf6abcfaca23411401846fe05
Sihan Wang <sw5796@nyu.edu>
2024-10-12 19:12:25 -0500

    fix syntax issues

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
 const axiosInstance = axios.create({
   baseURL: process.env.BACKEND_URL || 'http://localhost:5000',
