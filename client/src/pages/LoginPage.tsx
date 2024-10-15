4b04bea8ff7d40b4ab48d76b2d704c204ed280d9
Sihan Wang <sw5796@nyu.edu>
2024-10-14 23:14:15 -0500

    fix form ts syntax and login and auth logic

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import PrototypeForm from '../forms/PrototypeForm';
+import PrototypeForm, {Field} from '../forms/PrototypeForm';
 import { useDispatch, useSelector } from 'react-redux';
-import { login } from '../features/user/userSlice'; 
-import { RootState, AppDispatch } from '../app/store'; 
+import { login } from '../features/user/userSlice';
+import { RootState, AppDispatch } from '../app/store';
 
 interface LoginFormInputs {
   username: string;
@@ -14,13 +14,15 @@ interface LoginFormInputs {
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
   const dispatch: AppDispatch = useDispatch();
-
   const { loginStatus, error } = useSelector((state: RootState) => state.user);
   const methods = useForm<LoginFormInputs>();
-
   const onSubmit: SubmitHandler<LoginFormInputs> = (data) => {
     dispatch(login(data));
   };
+
+  useEffect(() => {
+    console.log(loginStatus, error);
+  })
 
   useEffect(() => {
     if (loginStatus === 'succeeded') {
@@ -28,7 +30,7 @@ const LoginPage: React.FC = () => {
     }
   }, [loginStatus, navigate]);
 
-  const fields = [
+  const fields: Field<LoginFormInputs>[] = [
     {
       name: 'username',
       label: 'Username',
@@ -49,7 +51,7 @@ const LoginPage: React.FC = () => {
       <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
       {error && (
         <div className="mb-4 text-red-600 text-center">
-          {error}
+          {JSON.stringify(error)}
         </div>
       )}
       <PrototypeForm
